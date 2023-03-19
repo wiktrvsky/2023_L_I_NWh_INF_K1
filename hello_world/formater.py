7c4811e8130328cffebbc1b9211b0ec1aa655c81
wiktrvsky <wrx81681@student.wsb.wroclaw.pl>
2023-03-19 13:55:41 +0100

    Poprawa JSONa + testy

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,4 +1,3 @@
-
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
@@ -21,7 +20,7 @@ def get_formatted(msg, imie, format):
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
+    return ('{ "imie":"' + imie + '", "msg":"' +
             msg + '"}')
 
 
